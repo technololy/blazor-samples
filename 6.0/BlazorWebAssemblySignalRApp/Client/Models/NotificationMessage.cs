9f57e11270b2414732d36cd9c6f3197f47adf6c3
ololadeoyebanji <loladeking@hotmail.com>
2023-03-05 21:03:48 +0000

    wip

---
+++ 6.0/BlazorWebAssemblySignalRApp/Client/Models/NotificationMessage.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace BlazorWebAssemblySignalRApp.Client.Models
+{
+	public class NotificationMessage
+	{
+        public string UserId { get; set; }
+
+        public long RefId { get; set; }
+
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+        public string Type { get; set; }
+        public object Data { get; set; }
+        public DateTime DateAdded { get; set; }
+    }
+}
+
